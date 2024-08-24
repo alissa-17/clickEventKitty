20666934d64939c5e163bc8fba9501ae38dafa88
Alissa <alissa.work17@aol.com>
2024-08-24 08:17:05 -0400

    editing button styles

--- js/script.js
+++ js/script.js
@@ -5,8 +5,17 @@ var cat = document.querySelector(".cat");
 // console.log(cat.innerHTML)
 
 button.addEventListener("click", function() {
-    button.innerText = "It's working.";
+   if(cat.classList.contains("show")){
+    cat.classList.remove("show");
+    button.innerText = "Wait, come back!";
+    cat.classList.add("disappear");
+   // console.log("Yes.");
+   }else{
     cat.classList.add("show");
+    button.innerText = "Shoo, cat!!";
+    cat.classList.remove("disappear");
+   // console.log("No.")
+   }
 }); 
 // console.log(button.innerHTML);
 
